1cab50e69244198edfa6f8c984bb22985f0f05b8
Apil KC <146890928+apilkc@users.noreply.github.com>
2026-01-18 15:17:13 -0500

    Update procedural-city.ts

--- data/experiments/procedural-city.ts
+++ data/experiments/procedural-city.ts
@@ -7,5 +7,5 @@ export const data: UpdateCardProps = {
   date: "DEMO",
   description: "Converting a map to a poster using Three.js and Perlin noise. Exploring the scale of digital concrete.",
   link: "https://github.com/originalankur/maptoposter",
-  image: ""
+  image: "data/experiments/pokhara_terracotta_20260118_134522.png"
 };
