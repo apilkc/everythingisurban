c7203855f74eca805fdc9c811062a012d73cca71
Apil KC <146890928+apilkc@users.noreply.github.com>
2026-06-20 22:25:20 +0530

    Update index.ts

--- data/experiments/index.ts
+++ data/experiments/index.ts
@@ -1,6 +1,6 @@
 import { UpdateCardProps } from '../../types';
 import { data as exp1 } from './procedural-city';
-import { data as exp2 } from './markitdown_card'; // Import the new card data
+import { data as exp2 } from './markitdown_card';
 
 export const experiments: UpdateCardProps[] = [
   exp1
